df70eff1755dfe1bb12e01188126f5a89b798467
flyingtatta <47095611+flyingtatta@users.noreply.github.com>
2021-03-28 17:32:36 +0530

    Update main.dart

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    var questions = ["What's your fav color", "What's your fav animal"];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text('The Question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
